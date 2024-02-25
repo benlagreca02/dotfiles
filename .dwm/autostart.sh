ec2a3cbf95dfd6912ad125529326fbef8288d3b9
Benjamin LaGreca <benlagreca02@gmail.com>
2024-02-25 17:47:03 -0500

    autostart script to spin stuff up, must have autostart patch for dwm

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# configure resolution settings
 xrandr -s 1920x1080 &
+# wallpaper selector
 nitrogen --set-zoom-fill --random &
+# compositor, how do windows look
 picom --experimental-backends &
 dwmblocks &
+# notifications
+dunst &
 
